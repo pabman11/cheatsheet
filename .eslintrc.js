63f4d0d405c12e6b0dcf37dc9348378f45f069d8
Pablo Fornali <pablofornalimanzanas@gmail.com>
2022-01-14 15:15:22 +0100

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["plugin:react/recommended", "airbnb", "prettier"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "no-unused-vars": "warn",
+  },
+};
