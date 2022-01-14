acc7e8cb177aa285c0904581a3c505c6c3dfb2ba
Pablo Fornali <pablofornalimanzanas@gmail.com>
2022-01-14 15:59:14 +0100

    Added react-responsive-modal module

--- src/components/Alias/index.jsx
+++ src/components/Alias/index.jsx
@@ -1,21 +1,9 @@
-import Popup from "../Helpers/Popup";
-import { useEffect, useState } from "react";
-import Aliases from "../aliases";
-
-export default function Alias({ alias, command, explain, link }) {
-
-  function copyToClipboard(command) {
-    navigator.clipboard.writeText(command);
-
-    Aliases.setTextShown("Copied to clipboard! ${command}");
-    Aliases.setIsOpen(true);
-  }
-
+export default function Alias({ alias, command, explain, link,customClickEvent }) {
   return (
     <article
       className={`grid grid-cols-4 gap-3 odd:bg-red-200 even:bg-red-400 hover:bg-blue-200`}
       key={alias}
-      onClick={() => copyToClipboard(alias)}
+      onClick={customClickEvent}
     >
       <code>{alias}</code>
       <span>{command}</span>
--- src/components/aliases/index.jsx
+++ src/components/aliases/index.jsx
@@ -1,13 +1,21 @@
-import reactDom from "react-dom";
 import { useEffect, useState } from "react";
 import getAliases from "../../services/getAliases";
 import Alias from "../Alias";
-import Popup from "../Helpers/Popup";
+
+import 'react-responsive-modal/styles.css';
+import { Modal } from 'react-responsive-modal';
 
 export default function Aliases() {
   const [aliases, setAliases] = useState([]);
   const [textShown, setTextShown] = useState("");
   const [isOpen, setIsOpen] = useState(false);
+
+
+  const onOpenModal = (text) => {
+    setIsOpen(true)
+    setTextShown(text)
+  };
+  const onCloseModal = () => setIsOpen(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -18,6 +26,13 @@ export default function Aliases() {
     fetchData();
     
   }, []);
+
+  const copyToClipboard = (command) => {
+    navigator.clipboard.writeText(command);
+    console.log("Copied to clipboard: " + command);
+    onOpenModal('Copied! 🎉'+ command);
+  }
+
   return (
     <>
       <h1>Aliases</h1>
@@ -26,15 +41,18 @@ export default function Aliases() {
           <Alias
             key={alias.alias}
             {...alias}
+            customClickEvent={() => { copyToClipboard(alias.alias); }}
           />
         ))}
       </section>
-      <Popup
-        selector="popup"
-        text={textShown}
-        isOpen={isOpen}
-        time={2000}
-      />
+      <Modal open={isOpen} onClose={onCloseModal} center classNames={{
+          modal: 'w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5',
+        }}
+>
+        <p>
+        {textShown}
+        </p>
+      </Modal>
     </>
   );
 }
