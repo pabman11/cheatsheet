6df2e185e302d5b336ffd3409d62658068bb7644
Pablo Fornali <pablo@magestio.com>
2022-01-24 08:09:53 +0100

    Possibly Fix Hook Error

--- src/services/getListofAliasFiles/index.jsx
+++ src/services/getListofAliasFiles/index.jsx
@@ -1,27 +1,15 @@
 import { octokit } from "../../utils/oktokit";
 import { useEffect, useState } from "react";
 
-export default function getListOfAliasFiles(){
+export default async function getListOfAliasFiles(){
 
-    const [aliasFiles, setAliasFiles] = useState([]);
-
-    useEffect(() => {
-        async function onLoad() {
-          await octokit.request(
-             'GET /repos/{owner}/{repo}/contents/{path}', {
-                owner: 'pabman11',
-                repo: 'cheatsheet',
-                path: `src/documents/`
-          })
-          .then(function(res){console.log(res);setAliasFiles(res.data)})
-          .catch(err => console.log(err));
-       }
-       onLoad();
-    },[])
-
-    return (
-        aliasFiles
-    )
-    
+    await octokit.request(
+        'GET /repos/{owner}/{repo}/contents/{path}', {
+        owner: 'pabman11',
+        repo: 'cheatsheet',
+        path: `src`
+    })
+    .then(res => res.data)
+    .catch(err => console.log(err));
 
 }
