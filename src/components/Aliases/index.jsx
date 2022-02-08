2b17734f25f9660abd2947cf32fd9a179ada9000
Pablo Fornali <pablofornalimanzanas@gmail.com>
2022-02-08 20:28:49 +0100

    Delete Modal and copy event

--- src/components/Aliases/index.jsx
+++ src/components/Aliases/index.jsx
@@ -1,34 +1,21 @@
 import { React, useEffect, useState, useContext } from 'react'
 
 import { Context } from '../../Context/Context'
-import { Modal } from 'react-responsive-modal'
 import { nanoid } from 'nanoid'
 
 import Alias from '../Alias'
 
-import 'react-responsive-modal/styles.css'
-
 export default function Aliases () {
     const [aliases, setAliases] = useState([])
-    const [textShown, setTextShown] = useState('')
-    const [isOpen, setIsOpen] = useState(false)
+    const [aliasType, setAliasType] = useState({ type: 'alias' })
     const [darkMode, aliasToShow, showAlias, activateDarkMode] = useContext(Context)
 
     useEffect(() => {
         if (aliasToShow.file) {
             setAliases(aliasToShow.file)
+            setAliasType(aliasToShow.type)
         }
     }, [aliasToShow])
-    const onOpenModal = (text) => {
-        setIsOpen(true)
-        setTextShown(text)
-    }
-    const onCloseModal = () => setIsOpen(false)
-
-    const copyToClipboard = (command) => {
-        navigator.clipboard.writeText(command)
-        onOpenModal(`🍨 Copied to clipboard 🍨 - ${command}`)
-    }
 
     return (
         <>
@@ -40,27 +27,9 @@ export default function Aliases () {
                         command={alias.command}
                         explain={alias.explain}
                         link={alias.link}
-                        customClickEvent={() => {
-                            copyToClipboard(alias.alias)
-                        }}
                     />
                 ))}
             </section>
-            <Modal
-                open={isOpen}
-                onClose={onCloseModal}
-                top
-                classNames={{
-                    modal:
-            'w-100 md:w-1/3 lg:w-1/4 xl:w-1/5 bg-green-100 rounded-lg shadow-lg',
-                    modalAnimationIn: 'slideToTop',
-                    modalAnimationOut: 'fadeOut'
-                }}
-                animationDuration={500}
-                animationOutDuration={200}
-            >
-                <p>{textShown}</p>
-            </Modal>
         </>
     )
 }
